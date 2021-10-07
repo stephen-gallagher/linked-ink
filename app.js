911c41f1e60b6930f11a09287b79df82160a6865
Stephen Gallagher <sgallagher92@gmail.com>
2021-10-07 16:50:11 +0200

    updated app.js with path

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
@@ -83,7 +84,18 @@ app.use("/api/auth", auth);
 const crud = require("./routes/crud");
 app.use("/api/crud", crud);
 
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
 
 module.exports = app;
